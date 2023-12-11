6e60b581b02ed675158ff57a377fd5b460589759
andreidavid8152 <andreidavid3399@gmail.com>
2023-12-10 22:09:21 -0500

    Se sube el API a Azure

--- Program.cs
+++ Program.cs
@@ -49,11 +49,13 @@ builder.Services.AddDbContext<ReservacionesDbContext>(option =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
 
 app.UseAuthentication();
